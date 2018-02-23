99e7a6f02506a1de7e6ed530d3e5aed09468f5e6
Caio Marinho <caiomarinho8@gmail.com>
2018-02-23 11:08:02 -0300

    implementando sistema individual para a loja

--- app/mixins/CustomContextMixin.py
+++ app/mixins/CustomContextMixin.py
@@ -101,15 +101,14 @@ class DashboardMixin(ContextMixin):
         notificacoes = Notification.objects.all()
         locations = Location.objects.all()
         bairros = Bairro.objects.all()
-        estabelecimentos = Estabelecimento.objects.all()
+        # estabelecimentos = Estabelecimento.objects.all()
         pontos = Ponto.objects.all()
         kwargs['motoristas'] = motoristas
         kwargs['pedidos'] = pedidos
         kwargs['users'] = users
-        kwargs['estabelecimentos'] = estabelecimentos
+        # kwargs['estabelecimentos'] = estabelecimentos
         kwargs['bd_space'] = float(float(float(
-            len(motoristas) + len(pedidos) + len(users) + len(notificacoes) + len(locations) + len(bairros) + len(
-                estabelecimentos) + len(pontos)) / 10000) * 100)
+            len(motoristas) + len(pedidos) + len(users) + len(notificacoes) + len(locations) + len(bairros) + len(pontos)) / 10000) * 100)
         kwargs['num_pedidos_entregues'] = Pedido.objects.filter(created_at__month=datetime.now().month,
                                                                 is_complete=True)
         kwargs['pedidos_do_mes'] = Pedido.objects.filter(created_at__month=datetime.now().month).order_by('-created_at')
@@ -121,7 +120,7 @@ class DashboardMixin(ContextMixin):
         kwargs['motoristas_online'] = Motorista.objects.filter(is_online=True)
         kwargs['motoristas_livres'] = Motorista.objects.filter(is_online=True, ocupado=False)
         kwargs['motoristas_ocupados'] = Motorista.objects.filter(is_online=True, ocupado=True)
-        kwargs['lojas'] = Estabelecimento.objects.all().order_by('-created_at')
+        # kwargs['lojas'] = Estabelecimento.objects.all().order_by('-created_at')
         kwargs['pontos_mes'] = Ponto.objects.filter(created_at__month=now.month).order_by('bairro')
         kwargs['pontos_all'] = Ponto.objects.all().order_by('bairro')
         kwargs['address_all'] = Ponto.objects.all().order_by('endereco')
